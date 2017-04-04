e30995b7d2364cba07a03bd8684866d4bb84e2ea
Vincent Gutierrez <vincentgg89@gmail.com>
2017-04-04 10:57:17 -0500

    Fix an issue when editing a playlist

--- app/assets/javascripts/playlists.js
+++ app/assets/javascripts/playlists.js
@@ -1,28 +1,30 @@
 $(document).ready(function() {
-  function typeahead_initializer() {
-    var spotifyTracks = new Bloodhound({
-      identify: function(o) { return o.tracks.items.id; },
-      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
-      queryTokenizer: Bloodhound.tokenizers.whitespace,
-      prefetch: 'https://api.spotify.com/v1/search?q=muse&type=track&market=MX&limit=10&offset=0',
-      remote: {
-        url: 'https://api.spotify.com/v1/search?q=%QUERY&type=track&market=MX&limit=10&offset=0',
-        wildcard: '%QUERY',
-        transform: function(response) {
-          return response.tracks.items;
-        }
-      }
-    });
 
-    $('.typeahead').typeahead(null, {
+  var spotifyTracks = new Bloodhound({
+    identify: function(o) { return o.tracks.items.id; },
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: 'https://api.spotify.com/v1/search?q=muse&type=track&market=MX&limit=10&offset=0',
+    remote: {
+      url: 'https://api.spotify.com/v1/search?q=%QUERY&type=track&market=MX&limit=10&offset=0',
+      wildcard: '%QUERY',
+      transform: function(response) {
+        return response.tracks.items;
+      }
+    }
+  });
+
+  function typeahead_initializer(trackElement, bhConfig) {
+    $(trackElement).typeahead(null, {
       name: 'name',
       display: 'name',
-      source: spotifyTracks
+      source: bhConfig
     });
   }
 
   $('#add-track-button').data('association-insertion-method', 'prepend').data('association-insertion-node', '#tracks-container');
   $('#tracks-container').on('cocoon:after-insert', function(e, insertedItem) {
-    typeahead_initializer();
+    typeahead_initializer($(insertedItem).find('.typeahead'), spotifyTracks);
   });
+  typeahead_initializer($('.typeahead'), spotifyTracks);
 });
